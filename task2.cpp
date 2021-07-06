cb783cc734e32db8257060790247ed6530d54e56
Faccy <951952627@qq.com>
2021-07-06 17:29:51 +0800

    utf

--- task2.cpp
+++ task2.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <vector>
 using namespace std;
 template<class T>
@@ -37,12 +37,12 @@ void Output(vector<T> v) {
 
 int main()
 {
-	//×Ö·û²âÊÔ
+	//å­—ç¬¦æµ‹è¯•
 	vector<char> myv = { 'h','i','a','c','e','d','b','g','f' };
 	Output(myv);
 	mySort(myv, 0, 8);
 	Output(myv);
-	//ÕûÊý²âÊÔ
+	//æ•´æ•°æµ‹è¯•
 	vector<int> myv2 = { 3,5,2,7,3,9,1,32,8 };
 	Output(myv2);
 	mySort(myv2, 0, 8);
